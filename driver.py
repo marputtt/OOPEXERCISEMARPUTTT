1005cd7a792584b6a70de519abc004f2203bb829
marputtt <marsyaputra22@gmail.com>
2023-12-05 14:14:01 +0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,13 +4,13 @@ def createItemListMP():
     foodList = []
     numItems = int(input("How many items will you order today? "))
     if numItems < 1:
-        print("Number of items must be at least 1.")
+        print("You need to order at least 1 item.")
         return createItemListMP()
     for i in range(1, numItems + 1):
         foodName = input(f"Item #{i}-\nEnter food: ")
         amount = float(input(f"Enter amount of pounds: "))
         if amount <= 0:
-            print("Amount of pounds must be greater than 0.")
+            print("The pounds must be greater than 0.")
             continue
         foodItem = fi.FoodItemMP(foodName, amount)
         foodItem.priceListMP()
